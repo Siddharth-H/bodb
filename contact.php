791b161feee81f3c0005cf35099c04691af525a3
sidd <siddhantkhanna96@gmail.com>
2017-09-25 17:05:41 +0530

    first comit

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Welcome to BODb !!</title>
+<title>Welcome to BODb sid !!</title>
 <link href='http://fonts.googleapis.com/css?family=Droid+Serif' rel='stylesheet' type='text/css'>
 <link href="css/styles.css" rel="stylesheet" type="text/css" />
 </head>
